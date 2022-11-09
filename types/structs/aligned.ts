e5ff322b972d424f07ca7a182db5f567f1bd6e54
eliassjogreen <eliassjogreen1@gmail.com>
2022-11-09 22:08:35 +0100

    feat: struct types, custom aligned base struct

--- types/structs/aligned.ts
+++ types/structs/aligned.ts
@@ -1,11 +1,18 @@
 import { AlignedType, InnerType, SizedType } from "../../types.ts";
 import { Struct } from "./mod.ts";
 
-export class PackedStruct<
+export class AlignedStruct<
   T extends Record<string, SizedType<unknown> | AlignedType<unknown>>,
   V extends { [K in keyof T]: InnerType<T[K]> },
 > extends Struct<T, V> {
-  constructor(types: T, byteAlign: number = 1) {
+  constructor(
+    types: T,
+    byteAlign: number = Math.max(
+      ...Object.values(types).map((type) =>
+        "byteAlign" in type ? type.byteAlign : type.byteLength
+      ),
+    ),
+  ) {
     const typeRecord: Record<
       string,
       [number, SizedType<unknown> | AlignedType<unknown>]
@@ -15,9 +22,17 @@ export class PackedStruct<
     const typeEntries = Object.entries(types);
     for (let index = 0; index < typeEntries.length; index++) {
       const [key, type] = typeEntries[index];
-      typeRecord[key] = [byteLength, type];
-      byteLength += type.byteLength;
+      const typeAlign = "byteAlign" in type ? type.byteAlign : type.byteLength;
+      const typePadding = (typeAlign - (byteLength % typeAlign)) %
+        typeAlign;
+      const typeOffset = byteLength + typePadding;
+
+      typeRecord[key] = [typeOffset, type];
+      byteLength = typeOffset + type.byteLength;
     }
+
+    byteLength = byteLength +
+      (byteAlign - (byteLength % byteAlign)) % byteAlign;
 
     super(
       typeRecord as { [K in keyof T]: [number, T[K]] },
--- types/structs/mod.ts
+++ types/structs/mod.ts
@@ -1,2 +1,75 @@
-export * from "./abstract.ts";
+import { AlignedType, InnerType, SizedType } from "../../types.ts";
+
+export class Struct<
+  T extends Record<string, SizedType<unknown> | AlignedType<unknown>>,
+  V extends { [K in keyof T]: InnerType<T[K]> },
+> implements AlignedType<V> {
+  byteLength: number;
+  byteAlign: number;
+  typeRecord: { [K in keyof T]: [number, T[K]] };
+
+  constructor(
+    types: { [K in keyof T]: [number, T[K]] },
+    byteLength = Math.max(
+      ...Object.values(types).map(([offset, type], index, array) =>
+        offset + (index === array.length - 1 ? type.byteLength : 0)
+      ),
+    ),
+    byteAlign = Math.max(
+      ...Object.values(types).map(([, type]) =>
+        "byteAlign" in type ? type.byteAlign : type.byteLength
+      ),
+    ),
+  ) {
+    this.typeRecord = types;
+    this.byteLength = byteLength;
+    this.byteAlign = byteAlign;
+  }
+
+  read(view: DataView, offset: number): V {
+    const object: Record<string, unknown> = {};
+
+    for (const [key, [typeOffset, type]] of Object.entries(this.typeRecord)) {
+      object[key] = type.read(view, offset + typeOffset);
+    }
+
+    return object as V;
+  }
+
+  write(view: DataView, offset: number, value: V) {
+    for (const [key, [typeOffset, type]] of Object.entries(this.typeRecord)) {
+      type.write(view, offset + typeOffset, value[key]);
+    }
+  }
+
+  object(view: DataView, offset: number): V {
+    const object = {};
+
+    Object.defineProperties(
+      object,
+      Object.fromEntries(
+        Object.keys(this.typeRecord).map(
+          (key) => {
+            return [key, {
+              configurable: true,
+              enumerable: true,
+
+              get: () => {
+                const [typeOffset, type] = this.typeRecord[key];
+                return type.read(view, offset + typeOffset);
+              },
+              set: (value) => {
+                const [typeOffset, type] = this.typeRecord[key];
+                type.write(view, offset + typeOffset, value);
+              },
+            }];
+          },
+        ),
+      ),
+    );
+
+    return object as V;
+  }
+}
+
 export * from "./packed.ts";
