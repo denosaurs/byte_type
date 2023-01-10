72a8fbd16337a20cb7fc1ba6608a4fc76430821a
MierenManz <christiaanmens@gmail.com>
2023-01-10 20:44:33 +0100

    feat: add string benchmark

---
+++ benchmarks/string.ts
@@ -0,0 +1,23 @@
+import { FixedUTF8String } from "../types/string/mod.ts";
+
+const stringThing = new FixedUTF8String(12);
+
+const ab = new TextEncoder().encode("Hello World!").buffer;
+const dt = new DataView(ab);
+
+
+Deno.bench("no-op", () => {});
+
+Deno.bench({
+  name: "Read",
+  fn: () => {
+    stringThing.read(dt);
+  }
+});
+
+Deno.bench({
+  name: "Write",
+  fn: () => {
+    stringThing.write( "hello world!", dt);
+  }
+});
