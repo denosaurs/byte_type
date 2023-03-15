6c0f3e936dd1df24fed62779704f5e0c6538e918
MierenManz <christiaanmens@gmail.com>
2023-03-15 22:06:43 +0100

    fmt

--- types/string/fixed_length.ts
+++ types/string/fixed_length.ts
@@ -12,14 +12,22 @@ export class FixedLengthString implements SizedType<string> {
 
   read(dataView: DataView, byteOffset = 0): string {
     return decoder.decode(
-      new Uint8Array(dataView.buffer, dataView.byteOffset + byteOffset, this.byteLength - byteOffset),
+      new Uint8Array(
+        dataView.buffer,
+        dataView.byteOffset + byteOffset,
+        this.byteLength - byteOffset,
+      ),
     );
   }
 
   write(value: string, dataView: DataView, byteOffset = 0) {
     encoder.encodeInto(
       value,
-      new Uint8Array(dataView.buffer, dataView.byteOffset + byteOffset, this.byteLength - byteOffset),
+      new Uint8Array(
+        dataView.buffer,
+        dataView.byteOffset + byteOffset,
+        this.byteLength - byteOffset,
+      ),
     );
   }
 }
