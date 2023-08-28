65b3cb8da09accbb8452a17d6ac6bef16090cbf5
MierenManz <christiaanmens@gmail.com>
2023-08-28 17:25:23 +0200

    rename

--- types/types.ts
+++ types/types.ts
@@ -4,8 +4,8 @@ export interface TypeOptions {
 
 export type InnerType<T> = T extends Type<infer I> ? I : never;
 export interface Type<T> {
-  read(dataView: DataView, byteOffset?: TypeOptions): T;
-  write(value: T, dataView: DataView, byteOffset?: TypeOptions): void;
+  read(dataView: DataView, options?: TypeOptions): T;
+  write(value: T, dataView: DataView, options?: TypeOptions): void;
 }
 
 export interface SizedType<T> extends Type<T> {
@@ -17,5 +17,5 @@ export interface AlignedType<T> extends SizedType<T> {
 }
 
 export interface ViewableType<T> {
-  view(dataView: DataView, byteOffset?: TypeOptions): T;
+  view(dataView: DataView, options?: TypeOptions): T;
 }
