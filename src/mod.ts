a05ca4d9ba194b8295c9023a3d0c9c18c1250c09
MierenManz <christiaanmens@gmail.com>
2023-10-28 14:25:15 +0200

    fmt

--- src/mod.ts
+++ src/mod.ts
@@ -1,14 +1,20 @@
 export * from "./types/mod.ts";
 export * from "./util.ts";
+
 export * from "./array/mod.ts";
 export * as Array from "./array/mod.ts";
+
 export * from "./bitflags/mod.ts";
 export * as Bitflags from "./bitflags/mod.ts";
+
 export * from "./primitives/mod.ts";
 export * as Primitives from "./primitives/mod.ts";
+
 export * from "./string/mod.ts";
 export * as String from "./string/mod.ts";
+
 export * from "./varint/mod.ts";
 export * as VarInt from "./varint/mod.ts";
+
 export * from "./compound/mod.ts";
 export * as Compound from "./compound/mod.ts";
