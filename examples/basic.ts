1ae1743949dfdacc75e866bdeaba1a0dcaa3eae6
MierenManz <christiaanmens@gmail.com>
2023-10-28 14:31:42 +0200

    rewrite basic example

--- examples/basic.ts
+++ examples/basic.ts
@@ -1,8 +1,9 @@
-import { PackedStruct, u32, u8 } from "../types/mod.ts";
+import { Struct, u32, u8 } from "../src/mod.ts";
 
-const o = new PackedStruct({ "b": u8, "a": u32 }).view(
-  new DataView(new ArrayBuffer(5)),
-  0,
-);
+const buffer = new ArrayBuffer(8);
+const dt = new DataView(buffer);
 
-console.log(o.valueOf());
+const o = new Struct({ "b": u8, "a": u32 });
+
+o.write({ b: 8, a: 32 }, dt);
+console.log(o.read(dt));
