139c870c98ac025314315ed5b6f7c647d91a7ef8
mierenmanz <christiaanmens@gmail.com>
2023-11-11 00:19:24 +0100

    plural

--- src/mod.ts
+++ src/mod.ts
@@ -2,7 +2,7 @@ export * from "./types/mod.ts";
 export * from "./util.ts";
 
 export * from "./array/mod.ts";
-export * as Array from "./array/mod.ts";
+export * as Arrays from "./array/mod.ts";
 
 export * from "./bitflags/mod.ts";
 export * as Bitflags from "./bitflags/mod.ts";
@@ -11,10 +11,10 @@ export * from "./primitives/mod.ts";
 export * as Primitives from "./primitives/mod.ts";
 
 export * from "./string/mod.ts";
-export * as String from "./string/mod.ts";
+export * as Strings from "./string/mod.ts";
 
 export * from "./varint/mod.ts";
-export * as VarInt from "./varint/mod.ts";
+export * as VarInts from "./varint/mod.ts";
 
 export * from "./compound/mod.ts";
-export * as Compound from "./compound/mod.ts";
+export * as Compounds from "./compound/mod.ts";
