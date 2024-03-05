59ff4043d9521ba54a328e2d4cdd94152c9f03ee
Skye <christiaanmens@gmail.com>
2024-03-05 23:13:40 +0100

    fix: benchmarks that got broken

--- benchmarks/string.ts
+++ benchmarks/string.ts
@@ -1,6 +1,6 @@
 import { Strings } from "../mod.ts";
 
-const stringThing = new Strings.FixedLength(12);
+const stringThing = new Strings.FixedLengthString(12);
 
 const ab = new TextEncoder().encode("Hello World!").buffer;
 const dt = new DataView(ab);
